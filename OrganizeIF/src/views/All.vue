09c6fd8c12a8ee8ba56861dfb07e6421b7c7195b
Bruno Gomes <brunog.vix@gmail.com>
2021-03-23 08:42:25 -0300

    botao voltar na listagem (#4)

--- OrganizeIF/src/views/All.vue
+++ OrganizeIF/src/views/All.vue
@@ -1,28 +1,33 @@
 <template>
     <ion-page>
         <ion-toolbar>
-            <ion-buttons>
-                <ion-back-button>
-                    
-                </ion-back-button>
+            <ion-buttons slot="start">
+                <ion-back-button default-href="/Lists"></ion-back-button>
             </ion-buttons>
+
+            <ion-icon :icon="ellipsisVertical" slot="end" class="text-2x1"></ion-icon>
         </ion-toolbar>
+
+        <ion-content>
+
+        </ion-content>
     </ion-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
-import { IonPage } from '@ionic/vue';
+import { IonPage, IonToolbar, IonButtons, IonBackButton, IonIcon, IonContent} from '@ionic/vue';
+import {ellipsisVertical} from 'ionicons/icons';
 
 export default defineComponent({
 
     components:{
-        IonPage
+        IonPage, IonToolbar, IonButtons, IonBackButton, IonIcon, IonContent
     },
 
     setup(){
         return {
-
+            ellipsisVertical
         }
     }
 
